9636a1e961ab8660e38bba7f150acb673613820a
MSI\Aadi Shah <shahaadesh5@gmail.com>
2019-06-22 18:41:26 -0300

    basic UI created

--- frontend/idataprep/src/app/app.module.ts
+++ frontend/idataprep/src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+ import {MatStepperModule} from '@angular/material/stepper';
+ import {MatButtonModule} from '@angular/material/button';
+ import {MatInputModule} from '@angular/material/input';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]

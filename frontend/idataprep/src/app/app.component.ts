2aa7eee63f192a5452712ce61205896e99ce23a6
MSI\Aadi Shah <shahaadesh5@gmail.com>
2019-06-22 21:16:14 -0300

    stepper with minimal data bindings and step controls

--- frontend/idataprep/src/app/app.component.ts
+++ frontend/idataprep/src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+
+export interface Category {
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'idataprep';
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  categories: Category[] = [
+    {name: 'Category 1'},
+    {name: 'Category 2'},
+    {name: 'Category 3'},
+  ];
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.categories.push({name: value.trim()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(category: Category): void {
+    const index = this.categories.indexOf(category);
+
+    if (index >= 0) {
+      this.categories.splice(index, 1);
+    }
+  }
+
 }
--- frontend/idataprep/src/app/app.module.ts
+++ frontend/idataprep/src/app/app.module.ts
@@ -8,6 +8,11 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
  import {MatStepperModule} from '@angular/material/stepper';
  import {MatButtonModule} from '@angular/material/button';
  import {MatInputModule} from '@angular/material/input';
+ import {MatChipsModule} from '@angular/material/chips';
+ import {MatSelectModule} from '@angular/material/select';
+ import {MatIconModule} from '@angular/material/icon';
+ import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +24,11 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatStepperModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatChipsModule,
+    MatSelectModule,
+    MatIconModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]

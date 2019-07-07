4aef26c2c6d8d31644970a39205b09c815c29452
Ravi-Tulsi-Zala <rv901764@dal.ca>
2019-07-07 16:28:21 -0300

    Some edits

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,10 @@
 from flask import Flask 
 from flask_socketio import SocketIO, send
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plot 
+import re
+import difflib 
 
 
 app = Flask(__name__)
@@ -14,9 +19,8 @@ def handleMessage(msg):
 def handleData(data):
 	print('Data')
 	print(data)
-	# print('Message: ' + msg)
-	# send(msg , broadcast=True)
 
 
+	
 if __name__ == '__main__':
     socketio.run(app)
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 function parseBytes(file, socket){
     var reader = new FileReader();
 
-    reader.onloadend = function(datafile) {
+    reader.onloadend = function(datafile) 
+    {
       console.log(datafile.target.result)
-      socket.emit('data', datafile.target.result);
+      socket.emit('loaddata', datafile.target.result);
     };
     
     reader.readAsArrayBuffer(file);

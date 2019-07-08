99eb60d5057db0eecdd6372d4696896ddaa0b21b
Sudarshan Suresh Srikant <sudarshan1994@gmail.com>
2019-07-08 13:33:16 -0300

    Sending header and column type updates to fron-end

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,16 @@
 from flask import Flask 
 from flask_socketio import SocketIO, send
+from pandas.api.types import is_numeric_dtype
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plot 
 import re
 import difflib 
 import csv
+import json
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-
 
 @socketio.on('message')
 def handleMessage(msg):
@@ -29,14 +30,51 @@ def read_the_csv(data,flag):
 			wr.writerow(csvList[i].split(','))
 			
 	myfile.close()
+	process_data(flag)
 
+
+def send_header(dataframe):
+	headers = dataframe.columns.to_list()
+	socketio.emit('headers',{'headers':headers})
+
+def get_dic_from_two_lists(keys, values):
+	return { keys[i] : values[i] for i in range(len(keys)) }
+
+
+def check_column_type(dataframe):
+	data_list = []
+	for columnName in dataframe:
+		if(is_numeric_dtype(dataframe[columnName])):
+			dict_keys = ['name', 'type']
+			dict_values = [columnName, 'numeric']
+			data = get_dic_from_two_lists(dict_keys, dict_values)
+			data_list.append(data)
+			featuresReceivedFromBackend = json.dumps(data_list)
+		else:
+			dict_keys = ['name', 'type']
+			dict_values = [columnName, 'categorical']
+			data = get_dic_from_two_lists(dict_keys, dict_values)
+			data_list.append(data)
+			featuresReceivedFromBackend = json.dumps(data_list)
+	socketio.emit('featuresReceivedFromBackend', featuresReceivedFromBackend)
+
+def process_data(flag):
 	if(flag):
-
 		dataframe_with_headers = pd.read_csv('uncleaned.csv',header=0)
-		print(dataframe_with_headers.columns)
+		send_header(dataframe_with_headers)
+		check_column_type(dataframe_with_headers)	
 
 	else :
+		names = []
 		dataframe_without_headers = pd.read_csv('uncleaned.csv')
+		print(len(dataframe_without_headers.columns))
+		for i in range(len(dataframe_without_headers.columns)):
+			names.append(i)
+		dataframe_with_headers = pd.read_csv('uncleaned.csv', names=names)
+		send_header(dataframe_with_headers)
+		check_column_type(dataframe_with_headers)
+
+
 
 
 

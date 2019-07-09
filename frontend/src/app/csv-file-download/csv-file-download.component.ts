2c44e2a20e45e910aaad2311ace0ff88ecabfce5
Samson Maconi <git@samsonmaconi.com>
2019-07-09 09:45:01 -0300

    [progress bar ] - waiting progress bars added

--- frontend/src/app/csv-file-download/csv-file-download.component.ts
+++ frontend/src/app/csv-file-download/csv-file-download.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class CsvFileDownloadComponent implements OnInit {
 
   @Output() completed = new EventEmitter<boolean>();
+  ready = false;
 
   constructor() { }
 
--- frontend/src/app/dataset-cleaning/dataset-cleaning.component.ts
+++ frontend/src/app/dataset-cleaning/dataset-cleaning.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatasetCleaningComponent implements OnInit {
 
+  ready = false;
+
   constructor() { }
 
   ngOnInit() {
